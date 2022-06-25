114e7d98e61668c93f8c49aefda7af0b9ab1608d
NIJIN P S <nijingithub@gmail.com>
2022-06-25 17:38:21 +0530

    Add code for Astroguru problem

---
+++ Assignment/astroGuru.js
@@ -0,0 +1,6 @@
+function astroguru(numChildren, spouse, location, job){
+    console.log(`You will be a ${job} in ${location} and married to ${spouse} with ${numChildren} children`);
+}
+
+
+astroguru(2, 'xyz', 'Kerala', 'SDE-2');
